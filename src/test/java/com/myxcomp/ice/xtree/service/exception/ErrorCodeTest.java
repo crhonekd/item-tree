54c50e91787e73cf96cebac08d9f0bb3471e7087
David Crhonek <crhonek.david@email.cz>
2026-05-24 08:02:14 +0200

    feat(copy): add 6 new ErrorCode values for copy operation

--- src/test/java/com/myxcomp/ice/xtree/service/exception/ErrorCodeTest.java
+++ src/test/java/com/myxcomp/ice/xtree/service/exception/ErrorCodeTest.java
@@ -1,11 +1,14 @@
 package com.myxcomp.ice.xtree.service.exception;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class ErrorCodeTest {
 
@@ -21,12 +24,31 @@ class ErrorCodeTest {
             "ITEM_NOT_FOUND",
             "HOME_FOLDER_NOT_FOUND",
             "INVALID_SEARCH_PARAMS",
-            "DATA_NOT_SERIALISABLE"
+            "DATA_NOT_SERIALISABLE",
+            "CANNOT_COPY_ROOT",
+            "DESTINATION_NOT_FOUND",
+            "DESTINATION_NOT_FOLDER",
+            "DESTINATION_NOT_IN_USER_FOLDER",
+            "COPY_INTO_DESCENDANT",
+            "COPY_TOO_LARGE"
     );
 
     @Test
     void enumExposesExactlyTheCodesTheDesignRequires() {
         List<String> actual = Arrays.stream(ErrorCode.values()).map(Enum::name).toList();
         assertThat(actual).containsExactlyInAnyOrderElementsOf(EXPECTED_NAMES);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "CANNOT_COPY_ROOT",
+        "DESTINATION_NOT_FOUND",
+        "DESTINATION_NOT_FOLDER",
+        "DESTINATION_NOT_IN_USER_FOLDER",
+        "COPY_INTO_DESCENDANT",
+        "COPY_TOO_LARGE"
+    })
+    void copyErrorCodesExist(String name) {
+        assertThatCode(() -> ErrorCode.valueOf(name)).doesNotThrowAnyException();
     }
 }
